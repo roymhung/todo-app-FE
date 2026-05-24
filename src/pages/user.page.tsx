d99d33390e93b717b91a474e43d1ee3e83415580
royhung <lemanhhung1555@gmail.com>
2026-05-24 07:44:58 +0700

    feat(user): implement user fetching and display list users in table

--- src/pages/user.page.tsx
+++ src/pages/user.page.tsx
@@ -1,48 +1,59 @@
-import { Table } from "antd";
+import { PlusCircleFilled, PlusCircleOutlined } from "@ant-design/icons";
+import { Button, Table } from "antd";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
 
 const UserPage = () => {
-  useEffect(() => {
-    axios.get("http://localhost:8080/users");
-  }, []);
+  const [users, setUsers] = useState<IUser[]>([]);
 
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
+  const fetchUsers = async () => {
+    const res = await axios.get("http://localhost:8080/users");
+
+    if (res?.data?.status === "success") {
+      setUsers(res.data.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const columns = [
     {
+      title: "Id",
+      dataIndex: "id",
+    },
+    {
       title: "Name",
       dataIndex: "name",
-      key: "name",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
+      title: "Email",
+      dataIndex: "email",
     },
   ];
 
   return (
     <div style={{ padding: "20px" }}>
-      <Table bordered dataSource={dataSource} columns={columns} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h3>Table Users</h3>
+        <Button type="primary" icon={<PlusCircleOutlined />}>
+          Add User
+        </Button>
+      </div>
+      <Table bordered dataSource={users} columns={columns} rowKey={"id"} />
     </div>
   );
 };
